38eb063f551c72d6a7ce82e361142eaa91d0159d
davidsousa2 <hellsorcere@gmail.com>
2025-05-01 13:04:35 -0300

    ajustes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("layouts/header.php"); ?>
 
 <div class="container mt-5">
-    <h1 class="text-center mb-4">Descubra seu signo zodiacal</h1>
+    <h1 class="text-center mb-4">Descubra seu signo</h1>
     <form action="show_zodiac_sign.php" method="POST" class="card p-4 mx-auto" style="max-width: 400px;">
         <div class="mb-3">
             <label for="data_nascimento" class="form-label">Data de Nascimento:</label>
